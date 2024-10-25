1ad5e73d6cc3fae18f6e9b920a800c0c5fa2aec1
dhivyasreekr <dhivyasreekr@gmail.com>
2024-10-25 18:40:06 +0530

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+require('dotenv').config(); //ensure environment variables are loaded
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB connected successfully');
+    } catch (err) {
+        console.error('MongoDB connection failed:', err.message);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+const disconnectDB = async () => {
+
+    try {
+        await mangoose.disconnect();
+        console.log("MongoDB disconnected successfully");
+    } catch (err) {
+        console.error('MongoDB connection failed', err.message);
+        process.exit(1);
+    }
+};
+
+module.experts = {connectDB, disconnectDB};
